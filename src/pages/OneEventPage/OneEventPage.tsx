8c6053f84db80b222fb6296438486225b94e808c
semilunaris <vlad.karpych@gmail.com>
2024-09-19 14:29:42 +0300

    UseCalendar

--- src/pages/OneEventPage/OneEventPage.tsx
+++ src/pages/OneEventPage/OneEventPage.tsx
@@ -8,6 +8,8 @@ import { getEventById } from "../../redux/api/eventsApi";
 import { UserList } from "../../components/UserList/UserList";
 import { FindUsers } from "../../components/FindUsers/FindUsers";
 import { unwrapResult } from "@reduxjs/toolkit";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const OneEventPage: React.FC = () => {
   const { id } = useParams<string>();
@@ -17,6 +19,18 @@ const OneEventPage: React.FC = () => {
   const [noUsersFound, setNoUsersFound] = useState(false);
   const [showUpdateEvent, setShowUpdateEvent] = useState(false);
   const dispatch = useAppDispatch();
+  const [showCalendar, setShowCalendar] = useState(true)
+// Предположим, что event может быть null, так что используем условную проверку
+const [selectDate, setSelectDate] = useState<Date | null>(null);
+
+// Убедитесь, что event не null перед обновлением состояния
+useEffect(() => {
+  if (event && event.date) {
+    setSelectDate(event.date);
+  }
+}, [event]);
+
+
 
   useEffect(() => {
     if (id) {
@@ -93,12 +107,16 @@ const OneEventPage: React.FC = () => {
     getUsers();
   };
 
+  
+  
   return (
     <div className={cls.trainingContainer}>
       <div className={cls.header}>
         <h3 className={cls.title}>{event.groupTitle}</h3>
-        <p className={cls.date}>
-          {new Date(event.date).toLocaleString("ru-RU", {
+       
+    
+        <p className={cls.date} onClick={()=>{setShowCalendar(!showCalendar)}}> 
+          {new Date(selectDate).toLocaleString("ru-RU", {
             day: "numeric",
             month: "long",
             year: "numeric",
@@ -106,6 +124,19 @@ const OneEventPage: React.FC = () => {
             minute: "2-digit",
           })}
         </p>
+     
+        {showCalendar && (
+        <DatePicker
+          selected={selectDate}
+          onChange={(date: any) => {
+            setSelectDate(date);
+            setShowCalendar(false); // Скрыть календарь после выбора даты
+          }}
+          dateFormat="dd MMMM yyyy"
+          inline
+        />
+      )}
+     
       </div>
       <div className={cls.participants}>
         <h4>Участники:</h4>
