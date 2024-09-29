8525698d7e73598179c52b8a6d4f1c3a7405fd9b
Liza <fildepiorsta@gmail.com>
2024-09-29 19:40:54 +0300

    userPage

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,21 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUsersByTelegramId } from "../../redux/api/usersApi";
+import { User } from "../../redux/types/types";
 
 const UserPage: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const { id } = useParams<string>();
+  const telegramId = Number(id);
 
-    const { id } = useParams<string>();
-    const telegramId = Number(id)
-    useEffect(() => {
-        if (telegramId) {
-          getUsersByTelegramId(telegramId)
-          .then(()=>console.log('getUser in UserPage'))
-            // .then((event) => setEvent(event))
-            // .catch((error) => console.error(error));
-        }
-      }, [telegramId]);
-    return <div>USSSSSEEEEEEEEEERRRRRRRRRRRRRR</div>;
-  };
-  
-  export default UserPage;
-  
+  useEffect(() => {
+    if (telegramId) {
+      getUsersByTelegramId(telegramId)
+        .then((user) => {
+          console.log("getUser in UserPage", user);
+          setUser(user);
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [telegramId]);
+
+  return (
+    <div>
+      {user ? (
+        <pre>{JSON.stringify(user, null, 2)}</pre> // test
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default UserPage;
