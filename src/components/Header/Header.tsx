3578f540f847ec410df0417a0ced7288cfbc4056
Liza <fildepiorsta@gmail.com>
2025-02-10 17:42:19 +0200

    click on logo and close burger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import cls from "./Header.module.scss";
 import { NavLink } from "react-router-dom";
+import { Navigate } from "react-big-calendar";
 
 const links = [
   { href: "#about", label: "О нас" },
@@ -27,9 +28,6 @@ export const Header = () => {
 
 
   const toggleMenu = () => {
-    if(isOpen){
-      setIsOpen(false)
-    }
     setIsOpen(!isOpen);
   };
   
@@ -37,13 +35,13 @@ export const Header = () => {
 
   return (
     <header className={cls.header}>
-      <div className={cls.logo}>Uken</div>
+      <div className={cls.logo} > <NavLink className={cls.logo} to="/" onClick={toggleMenu}>Uken</NavLink></div>
       <div className={cls.burger} onClick={toggleMenu}>
         <div className={cls.line}></div>
         <div className={cls.line}></div>
         <div className={cls.line}></div>
       </div>
-      <nav className={`${cls.nav} ${isOpen ? cls.open : ""}`}>
+      <nav className={`${cls.nav} ${isOpen ? cls.open : ""}`} onClick={toggleMenu}>
       {!isAdmin &&  <ul className={cls.navList}>
           {links.map((link) => (
             <li key={link.href}>
@@ -64,11 +62,11 @@ export const Header = () => {
           </li>
           </ul>}
        {isAdmin &&   <ul className={cls.navList}>
-        <NavLink className={cls.link} to="/" onClick={closeBurger}>
+        <NavLink className={cls.link} to="/" onClick={toggleMenu}>
               ГОЛОВНА
             </NavLink>
             <li>
-              <NavLink  className={cls.link}  to="/admin" onClick={closeBurger}>АДМІСТРАТОР</NavLink>
+              <NavLink  className={cls.link}  to="/admin" onClick={toggleMenu}>АДМІСТРАТОР</NavLink>
             </li>
           </ul>}       
       </nav>
